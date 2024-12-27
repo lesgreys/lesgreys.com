dd79f093a820f94545e42ef4005c733e056851a3
lesgreys <lester@lesgreys.com>
2024-12-27 12:01:50 -0500

    changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ const geistMono = localFont({
 
 export const metadata: Metadata = {
   title: "lesgreys.com",
-  description: "Personal side",
+  description: "Les Greys",
 };
 
 export default function RootLayout({
